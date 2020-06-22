e1f95b492277064edbbb0cee97735f7a6063e7ac
Lorenz <lorenz.kort@gmail.com>
2020-06-22 23:48:35 +0200

    Simplified query input

--- newItems/request_item.py
+++ newItems/request_item.py
@@ -1,4 +1,4 @@
-# import json library
+# import libraries
 import urllib.request
 import json
 import pandas as pd
@@ -25,22 +25,35 @@ def get_items(query_url):
     df = df[['id', 'url']]
     return df
 
-def notify(df, file_name, item_name, chat_id='-425371692'):
+def notify(df, file_name, keyword, chat_id='-425371692'):
     with open(file_name, 'r') as f:
         for ind in df.index:
             if any(df['id'][ind] in line for line in f):
                 pass # known id
             else:
-                print('New ' + item_name)
-                telegram_send_text('New ' + item_name + ': ' + df['url'][ind], chat_id)
+                print('New ' + keyword)
+                telegram_send_text('New ' + keyword + ': ' + df['url'][ind], chat_id)
                 break
     return
 
-def check(item_name='concept 2', query_url='https://www.marktplaats.nl/lrp/api/search?l1CategoryId=784&limit=100&offset=0&postcode=2012EG&query=concept%202&sortBy=SORT_INDEX&sortOrder=DECREASING&viewOptions=list-view', chat_id='-425371692'):
+def url_gen(keyword='', CategoryId='', TitleAndDescription=False):
+    if keyword != '':
+        keyword = str(keyword).replace(' ', '%20')
+    if CategoryId != '':
+        CategoryId = 'l1CategoryId=' + str(CategoryId)
+    if TitleAndDescription == True:
+        TitleAndDescription = '&searchInTitleAndDescription=true'
+    else:
+        TitleAndDescription = ''
+    url = 'https://www.marktplaats.nl/lrp/api/search?attributesById[]=32&attributesById[]=31&' + CategoryId + '&limit=100&offset=0&query=' + keyword + TitleAndDescription + '&sortBy=SORT_INDEX&sortOrder=DECREASING'
+    return url
+
+def check(keyword='concept 2', chat_id='-425371692', CategoryId='', TitleAndDescription=False):
     pi_dir = '/home/pi/Documents/Python/marktplaatsMaster/data/'
-    #mac_dir = ''
-    file_name = pi_dir + item_name.replace(' ','_').lower() + '_response.csv'
+    #mac_dir = '/Users/lorenzkort/Documents/Python/marktplaatsMaster/data/'
+    file_name = pi_dir + keyword.replace(' ','_').lower() + '_response.csv'
+    query_url = url_gen(keyword, CategoryId, TitleAndDescription)
     items_df = get_items(query_url) # get items
-    notify(items_df, file_name, item_name, chat_id) # mail new id's
+    notify(items_df, file_name, keyword, chat_id) # mail new id's
     items_df.to_csv(file_name) # save csv
     return
--- newItems/telegram_bot.py
+++ newItems/telegram_bot.py
@@ -4,7 +4,6 @@ def telegram_send_text(bot_message='Hey this is a text message', chat_id = '-425
     
     bot_token = '1132455575:AAFWwpwZ-qJUdabq-WbSHoK8nh7aPwMoVo4'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + bot_message
-
     response = requests.get(send_text)
 
     return response.json()
