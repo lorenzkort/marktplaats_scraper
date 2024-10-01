9af297e95fd5390f08553a398a60126825a1998c
lorenzkort <lorenz.kort@gmail.com>
2024-10-01 14:24:05 +0200

    Update telegram.py

--- jobs/telegram.py
+++ jobs/telegram.py
@@ -1,7 +1,7 @@
 import requests
 
-def send_text(msg='Hey this is a test message', chatId = '-1001797708509'):
-    bot_token = '1132455575:AAFWwpwZ-qJUdabq-WbSHoK8nh7aPwMoVo4'
+def send_text(msg='Hey this is a test message', chatId = '-999999999'):
+    bot_token = '1123409823:abcd'
     send_text = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatId}&parse_mode=Markdown&text={msg}"
     response = requests.get(send_text)
     return response.json()
